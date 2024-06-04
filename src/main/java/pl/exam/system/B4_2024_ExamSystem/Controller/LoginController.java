14c2fdb65f8ee423f3430f7ad6cefac28239b18e
Szałaj <szalajdaniel@interia.pl>
2024-06-04 14:41:03 +0200

    basic html pages, css added, login controller, main_page controller

---
+++ src/main/java/pl/exam/system/B4_2024_ExamSystem/Controller/LoginController.java
@@ -0,0 +1,49 @@
+package pl.exam.system.B4_2024_ExamSystem.Controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.exam.system.B4_2024_ExamSystem.Model.User;
+import pl.exam.system.B4_2024_ExamSystem.Service.UserService;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login(){
+        return "login_page";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password,
+                        RedirectAttributes redirectAttributes, HttpSession session) {
+        if (userService.authenticate(username, password)) {
+            User loggedInUser = userService.getUserByUsername(username);
+            Long userId = loggedInUser.getId();
+            String loggedRole = userService.getRole(userId);
+            session.setAttribute("loggedInUser", loggedInUser);
+            session.setAttribute("role", loggedRole);
+            redirectAttributes.addFlashAttribute("successMessage", "Zalogowano pomyślnie");
+            return "redirect:/main_page";
+        } else {
+            redirectAttributes.addFlashAttribute("errorMessage", "Błąd logowania");
+            return "redirect:/login_page";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        Object loggedInUser = session.getAttribute("loggedInUser");
+        if (loggedInUser != null) {
+            session.removeAttribute("loggedInUser");
+        }
+        return "redirect:/main_page";
+    }
+}
---
+++ src/test/java/pl/exam/system/B4_2024_ExamSystem/B42024ExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.exam.system.B4_2024_ExamSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B42024ExamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
